64a6078e0d1ca70108127d9be91a50c8db3af00b
htam261 <htam261@gmail.com>
2015-04-05 00:10:03 +0700

    trang chu moi tao

---
+++ tin247.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Tin tức cập nhật liên tục</title>
+	<link rel="stylesheet" type="text/css" href="css/tin247/style_v2.css">
+	<script src="lib/js/jquery.js" type="text/javascript"></script>
+	<script src="js/tin247/function.js" type="text/javascript"></script>
+</head>
+<body>
+	<!-- header -->
+	<div class="header">
+		<a class="tin247_logo" title="tin247.com" href="/tin/tin247.php">
+			<img title="http://www/tin247.com/" alt="http://www.tin247.com/" src="img/tin247/logo.png" />
+		</a>
+		<div class="top_head_ad">
+			<div id="tin247_banner_header_728x90">
+				<div onmouseover onmouseout class="first" style="width:720px; height:90px; display:inline-block; background:none;position: relative; font-size:0; text-align:center; overflow:hidden;" id="myadBan_16751">
+					<a title style target="_blank" href="http://ad.vatgia.com/a/b_click.php?data=eyJiW5faWQiOiIxNjc1MSIsInBvc1JiYW5fbGluayI6Imh0dHA6XC9cL3d3dy52YXRnaWEY29ob21lXC8iLCJjYXRfaWQiOiB9">
+						<img style="width:auto;" src="img/tin247/quang-cao.jpg" />
+					</a>
+					<a onmouseover="showBuyBannerMyad(this);" onmouseout="hideBuyBannerMyad(this);" title="Mua banner trên myadn.vn"
+						style="padding:0px; text-align:center; text-decoration:none; font-size:10px; z-index:5; position:absolute; top:0px; right: -118px;" target="_blank" href="http://myad.vn/home/banner_create.php?pos_id=0">
+						<img style="min-height:17px; min-width:142px; height:17px; max-height:17px;max-width:142px;" src="img/tin247/vat-gia.png"/>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div> <!-- end header -->
+	<div class="nav">
+		<ul class="nav_top">
+			<li><a title="trang chủ" href="/tin/tin247.php" class="active">Trang chủ</a></li>
+			<li><a title="Chính trị - Xã hội" href="/tin/tin247.php">Xã hội</a></li>
+			<li><a title="Tin thế giới" href="/tin/tin247.php">Tin thế giới</a></li>
+			<li><a title="Công nghệ" href="/tin/tin247.php">Công nghệ</a></li>
+			<li><a title="Văn hóa - giải trí" href="/tin/tin247.php">Văn hóa - Giải trí</a></li>
+			<li><a title="Giới trẻ" href="/tin/tin247.php">Giới trẻ</a></li>
+			<li><a title="Tình yêu giới tính" href="/tin/tin247.php">Tình yêu giới tính</a></li>
+			<li><a title="Tuổi teen" href="/tin/tin247.php">Tuổi teen</a></li>
+			<li><a title="Thể thao" href="/tin/tin247.php">Thể thao</a></li>
+			<li><a title="Pháp luật" href="/tin/tin247.php">Pháp luật</a></li>
+			<li><a title="Phái đẹp" href="/tin/tin247.php">Phái đẹp</a></li>
+			<li><a title="Ôtô xe máy" href="/tin/tin247.php">Ôtô- xe máy </a></li>
+			<li class="menu">
+				<a class="menu_btn" title="xem thêm" href="javascript:void(0)"></a>
+				<ul class="nav_bottom">
+					<li><a title="Tâm sự bạn đọc" href="#">Tâm sự bạn đọc</a></li>
+					<li><a title="Review Sản phẩm" href="#">Review Sản phẩm</a></li>
+					<li><a title="Đời sống sức khỏe" href="#">Đời sống sức khỏe</a></li>
+					<li><a title="Giáo dục" href="#">Giáo dục</a></li>
+					<li><a title="Khoa học" href="#">Khoa học</a></li>
+					<li><a title="Chuyện lạ" href="#">Chuyện lạ</a></li>
+					<li><a title="Nhà đất" href="#">Nhà đất</a></li>
+					<li><a title="Vui cười thư giản" href="#">Vui cười- Thư giản</a></li>
+					<li><a title="Kinh tế" href="#">Kinh tế</a></li>
+					<li><a title="Ẩm thực" href="#">Ẩm thực</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div><!-- end nav -->
+</body>
+</html>
